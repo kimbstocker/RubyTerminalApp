259588ee9b6059548672b782bac120beeaf80ecc
Kim Stocker <kimbstocker@gmail.com>
2021-12-14 00:20:47 +1100

    add medium and hard level quiz questions

--- src/classes/quiz.rb
+++ src/classes/quiz.rb
@@ -4,30 +4,66 @@ $single_digit_numbers = (1..9).to_a
 $double_digit_numbers = (10..99).to_a
 $triple_digit_numbers = (100..999).to_a
 
-class Quiz
+class EasyLevelQuiz
 
     attr_accessor :correct_answers
     attr_accessor :user_answers
 
-    def initialize(level)
-        @level = level
+    # def initialize(level)
+    def initialize
         @correct_answers = []
         @user_answers = []
     end
 
     def random_question
         
+        i = 1
+        while i <= 10 
+            first_num = $single_digit_numbers.sample
+            second_num = $single_digit_numbers.sample
+            third_num = $double_digit_numbers.sample
+            first_operator = $operators.sample
+            second_operator = $operators.sample
+            question = "Q#{i}) #{first_num} #{first_operator} #{second_num} #{second_operator} #{third_num}"
+            answer = eval("#{first_num} #{first_operator} #{second_num} #{second_operator} #{third_num}")
+            @correct_answers << answer
+            puts question
+            input = gets.chomp.to_i
+                if input == answer
+                    puts "Right"
+                else
+                    puts "Wrong"
+                end
+            @user_answers << input
+            i += 1
+        end
+        
+    end
+end
 
-        if @level == 1
+
+class MediumLevelQuiz
+
+    attr_accessor :correct_answers
+    attr_accessor :user_answers
+
+    # def initialize(level)
+    def initialize
+        @correct_answers = []
+        @user_answers = []
+    end
+
+    def random_question
+        
             i = 1
             while i <= 10 
-                @first_num = $single_digit_numbers.sample
-                @second_num = $single_digit_numbers.sample
-                @third_num = $double_digit_numbers.sample
-                @first_operator = $operators.sample
-                @second_operator = $operators.sample
-                question = "Q#{i}) #{@first_num} #{@first_operator} #{@second_num} #{@second_operator} #{@third_num}"
-                answer = eval("#{@first_num} #{@first_operator} #{@second_num} #{@second_operator} #{@third_num}")
+                first_num = $single_digit_numbers.sample
+                second_num = $double_digit_numbers.sample
+                third_num = $double_digit_numbers.sample
+                first_operator = $operators.sample
+                second_operator = $operators.sample
+                question = "Q#{i}) #{first_num} #{first_operator} #{second_num} #{second_operator} #{third_num}"
+                answer = eval("#{first_num} #{first_operator} #{second_num} #{second_operator} #{third_num}")
                 @correct_answers << answer
                 puts question
                 input = gets.chomp.to_i
@@ -39,7 +75,45 @@ class Quiz
                 @user_answers << input
                 i += 1
             end
-        end
+        
     end
 end
 
+class HardLevelQuiz
+
+    attr_accessor :correct_answers
+    attr_accessor :user_answers
+
+    # def initialize(level)
+    def initialize
+        @correct_answers = []
+        @user_answers = []
+    end
+
+    def random_question
+        
+            i = 1
+            while i <= 10 
+                first_num = $single_digit_numbers.sample
+                second_num = $double_digit_numbers.sample
+                third_num = $double_digit_numbers.sample
+                fourth_num = $triple_digit_numbers.sample
+                first_operator = $operators.sample
+                second_operator = $operators.sample
+                third_operator = $operators.sample
+                question = "Q#{i}) #{first_num} #{first_operator} #{second_num} #{second_operator} #{third_num} #{third_operator} #{fourth_num}"
+                answer = eval("#{first_num} #{first_operator} #{second_num} #{second_operator} #{third_num} #{third_operator} #{fourth_num}")
+                @correct_answers << answer
+                puts question
+                input = gets.chomp.to_i
+                    if input == answer
+                        puts "Right"
+                    else
+                        puts "Wrong"
+                    end
+                @user_answers << input
+                i += 1
+            end
+        
+    end
+end
--- src/main.rb
+++ src/main.rb
@@ -2,9 +2,17 @@ require_relative './classes/quiz.rb'
 require_relative './classes/scorecard.rb'
 
 puts "Choose level 1, 2 or 3"
-input = gets.chomp
-if input == "1"
-    quiz1 = Quiz.new(1)
+input = gets.chomp.to_i
+case input
+    when 1
+        quiz = EasyLevelQuiz.new
+        quiz.random_question
+    when 2
+        quiz = MediumLevelQuiz.new
+        quiz.random_question
+    when 3
+        quiz = HardLevelQuiz.new  
+        quiz.random_question 
 end
 
-quiz1.random_question
+
