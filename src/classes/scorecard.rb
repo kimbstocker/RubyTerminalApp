c30458cf9750e54ee2adcb2a0d5934ed2fa9adfc
Kim Stocker <kimbstocker@gmail.com>
2021-12-13 22:41:26 +1100

    create quiz class and random question method

---
+++ src/classes/scorecard.rb
@@ -0,0 +1,11 @@
+# class ScoreCard
+#     initialize(username, file_path)
+#     @username = name
+#     @file_path = file_path
+
+
+#     def print_scorecard
+#         if @@user_answers.length == 10
+#             @@user_answers.each do |answer|
+#             puts 
+#             print correct_answers
