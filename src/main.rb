9fe9286f760337b7e02856ed1d32fb97e7781aa8
Kim Stocker <kimbstocker@gmail.com>
2021-12-16 01:20:55 +1100

    add command line argument options for shortcut to start the quiz

--- src/main.rb
+++ src/main.rb
@@ -2,23 +2,29 @@ require 'colorize'
 require_relative './classes/quiz.rb'
 require_relative './classes/error-handling.rb'
 
-# if ARGV.length > 0 
-#     flag, *rest = ARGV 
-#     ARGV.clear
-#     case flag
-#     when '-help' || '-h'
-#         puts "Check the readme"
-#         exit
-#     when '-info'
-#         puts "Written by FX2021-02"
-#         exit
-#     when '-path'
-#         group = Group.new(rest[1] ||= "Default Group", rest[0])
-#     else 
-#      puts "Invalid argument, please check Readme file for more info"
-#      exit 
-#     end
-# end 
+if ARGV.length > 0 
+    flag, *rest = ARGV 
+    ARGV.clear
+    case flag
+    when '-help'
+        puts "-r username level:    to start the Quiz (username: your username, level: 1, 2 or 3 only)"
+        puts "-info:                to view history of the app and developer information"
+        exit
+    when '-info'
+        puts "This terminal application namely 'Mad Math Quiz' is written by Kim Stocker in December - 2021 for CoderAcademy Full-stack Developer Course - Term 1 assignment"
+        puts "This app was written in Ruby from scratch. It was finished in 1 week"
+        puts "For more info, please visit https://github.com/kimbstocker/KimStocker_T1A3"
+        exit
+    when '-r' 
+        if rest[1] == '1' || rest[1] == '2' || rest[1] == '3'
+        quiz = Quiz.new(rest[0], rest[1])
+            quiz.run_quiz
+        end
+    else 
+        puts "Invalid argument, please check Readme file for more info"
+        exit 
+    end
+end 
 
 
 while true
@@ -26,6 +32,7 @@ while true
         puts "Welcome to Mad Math Quiz! Let's get started with a username, enter anything and it will be your username for this application!"
         username = gets.chomp
         puts "Please enter 1 for easy, 2 for medium, 3 for hard level, q to quit!"
+        puts "(once continue, you will need to complete all 10 quiz questions before you could see quit option again)"
         user_choice = gets.chomp
         if user_choice == "q"
                 puts "Thank you and goodbye!"
